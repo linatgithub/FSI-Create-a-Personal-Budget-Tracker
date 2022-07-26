4ca20ee54f0399fac899847bf99976b42aaa08de
Lin <linlincc@gmail.com>
2022-07-26 14:31:56 -0400

    reverse order for weekly expense, while loop for annual expense

--- index.js
+++ index.js
@@ -14,17 +14,22 @@ let numberAnswer = parseFloat(stringAnswer)
 console.log(numberAnswer)
 */
 
-for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
-weeklyExpenses = weeklyExpenses + answer
+for (let i = weeklyExpenseQuestions.length - 1; i >= 0; i--) {
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
 }
 
 for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
     monthlyExpenses = monthlyExpenses + answer
-    }
+}
 
-    for (let i = 0; i < annualExpenseQuestions.length; i++) {
-        let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
-        annualExpenses = annualExpenses + answer
-        }
+let i = 0
+
+while (i < annualExpenseQuestions.length) {
+
+//for (let i = 0; i< annualExpenseQuestions.length; i++) {
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+    i++
+}
